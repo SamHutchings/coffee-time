60b19f19e13070bfea069a5f927ec27c87ba1202
Sam Hutchings <shutchings@opentable.com>
2019-05-06 21:21:01 +0100

    install cors on api

--- api/app.js
+++ api/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const recipeService = require("./recipe-service");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
